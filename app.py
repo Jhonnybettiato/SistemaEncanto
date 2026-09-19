66947fee1bbcba01606525782b86df412dfb9973
Jhonnybettiato <168930607+Jhonnybettiato@users.noreply.github.com>
2026-09-19 00:32:43 -0300

    Change header and sidebar background color opacity

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def aplicar_fundo_local(caminho_imagem):
 
             /* Quitar fondos blancos por defecto */
             [data-testid="stHeader"], [data-testid="stSidebar"] {{
-                background-color: rgba(255, 255, 255, 0.90) !important;
+                background-color: rgba(255, 255, 255, 0.10) !important;
             }}
 
             /* Contenedor principal con fondo limpio */
